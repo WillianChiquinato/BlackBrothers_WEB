6b19894647a4ab071a2d6955217c81a82d032b54
WillianChiquinato <willianchiquinato@hotmail.com>
2025-04-08 19:54:46 -0300

    Primeiro commit - WEB

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SideBar from './components/sideBar'
+
+function App() {
+
+  return (
+    <SideBar />
+  )
+}
+
+export default App
---
+++ src/components/sideBar/index.jsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+    return(
+        <div>
+            <span>Aqui é a tela para fazer o menu lateral</span>
+        </div>
+    )
+}
